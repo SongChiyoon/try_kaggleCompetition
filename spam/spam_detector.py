1313290229e84d4a683e42948b06b3995f8da3c9
송치윤 <songcchiyoon@gmail.com>
2017-06-30 13:32:16 +0900

    no message

--- spam/spam_detector.py
+++ spam/spam_detector.py
@@ -12,4 +12,18 @@ df['v1'] = df['v1'].map(lambda x:1 if x == 'ham' else 0)
 df['words'] = df['v2'].map(lambda x: nltk.word_tokenize(x))
 
 feature_set = zip(df['v1'], df['words'])
-print(feature_set[1:5])
+
+
+len_fSet = len(feature_set)
+
+train_size = int(0.7 * len_fSet)
+test_size = int(0.3 * len_fSet)
+
+training_data = feature_set[:train_size]
+test_data = feature_set[train_size:test_size]
+print("training....")
+classifier = nltk.NaiveBayesClassifier.train(training_data)
+
+print("testing...")
+print("accuracy : ",nltk.classify.accuracy(test_data))
+
